7208ae8e847e5a671d80eed217ed66704011a7c4
u9g <git@u9g.dev>
2026-04-02 18:17:38 -0400

    Update date format: no seconds, 12h with lowercase am/pm and timezone

--- log-prompt.sh
+++ log-prompt.sh
@@ -4,7 +4,7 @@ PROMPT=$(echo "$INPUT" | jq -r '.prompt')
 SESSION_ID=$(echo "$INPUT" | jq -r '.session_id')
 CWD=$(echo "$INPUT" | jq -r '.cwd')
 DIR_NAME=$(basename "$CWD")
-DATETIME=$(date '+%Y-%m-%d %H:%M:%S')
+DATETIME=$(date '+%Y-%m-%d %I:%M%P %Z')
 
 mkdir -p ~/.claudeprompts/${DIR_NAME}
 echo -e "${DATETIME}\n${PROMPT}\n" >> ~/.claudeprompts/${DIR_NAME}/${SESSION_ID}.log
